e13a143c705debd99aced8dcb1a8bf57423ec3ec
conan513 <conan_513@hotmail.com>
2012-08-29 10:51:35 +0200

    update files

---
+++ characters_linux.sh
@@ -0,0 +1,4 @@
+# characters
+mysql -u root --password=123456 -h 127.0.0.1 --database=characters < "sql/characters.sql"
+mysql -u root --password=123456 -h 127.0.0.1 --database=characters < "sql_mr/custom_characters_tables.sql"
+
---
+++ realmd_linux.sh
@@ -0,0 +1,4 @@
+# realmd
+mysql -u root --password=123456 -h 127.0.0.1 --database=realmd < "sql/realmd.sql"
+mysql -u root --password=123456 -h 127.0.0.1 --database=realmd < "sql_mr/custom_realmd_tables.sql"
+
