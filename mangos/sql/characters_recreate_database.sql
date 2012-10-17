57a6294a9bc6947ce8def82704001856fb1c920f
ConanHUN <conan_513@hotmail.com>
2012-10-17 18:21:34 +0200

    SQL files added

---
+++ mangos/sql/characters_recreate_database.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS `characters` ;
+
+CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ mangos/sql/realmd_recreate_database.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS `realmd` ;
+
+CREATE DATABASE `realmd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
