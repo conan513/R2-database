0428ac503b94590cc391fe003081ba0212484504
ConanHUN <conan_513@hotmail.com>
2012-10-17 18:16:10 +0200

    Linux files moved into backup

--- characters_linux.sh
+++
@@ -1,3 +0,0 @@
-# characters
-mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=characters < "sql/characters.sql"
-mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=characters < "sql_mr/custom_characters_tables.sql"
--- realmd_linux.sh
+++
@@ -1,3 +0,0 @@
-# realmd
-mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=realmd < "sql/realmd.sql"
-mysql -u root --password=123456 -h 127.0.0.1 --port=3310 --database=realmd < "sql_mr/custom_realmd_tables.sql"
